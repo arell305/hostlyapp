0a7abe9c444dc58c7970fe7e8ff390efb155fa4e
David  Anusontarangkul <davidanusontarangkul@Davids-MacBook-Pro.local>
2024-08-31 16:47:02 -0700

    update tests

--- __tests__/frontend/pages/LoginPage.test.tsx
+++ __tests__/frontend/pages/LoginPage.test.tsx
@@ -6,7 +6,6 @@ describe("Login Component", () => {
   it("renders the login component with heading", () => {
     render(<Login />);
 
-    // Check if the main heading is rendered
     const heading = screen.getByRole("heading", { name: /Login to Hostly/i });
     expect(heading).toBeInTheDocument();
   });
@@ -14,7 +13,6 @@ describe("Login Component", () => {
   it("renders Google login button", () => {
     render(<Login />);
 
-    // Check if the Google login button is rendered
     const googleButton = screen.getByRole("link", {
       name: /Continue with Google/i,
     });
@@ -24,7 +22,6 @@ describe("Login Component", () => {
   it("renders Apple login button", () => {
     render(<Login />);
 
-    // Check if the Apple login button is rendered
     const appleButton = screen.getByRole("link", {
       name: /Continue with Apple/i,
     });
@@ -34,7 +31,6 @@ describe("Login Component", () => {
   it("renders Talk With Sales button", () => {
     render(<Login />);
 
-    // Check if the Talk With Sales button is rendered
     const talkWithSalesButton = screen.getByRole("link", {
       name: /Talk With Sales/i,
     });
