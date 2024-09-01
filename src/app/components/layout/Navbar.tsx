5c7fb8b5ff80ebe2703b90a5ebfbdb60899b97e1
David  Anusontarangkul <davidanusontarangkul@Davids-MacBook-Pro.local>
2024-08-31 17:19:15 -0700

    add staging

--- src/app/components/layout/Navbar.tsx
+++ src/app/components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { TITLE } from "@/app/constants";
 
 const Navbar: React.FC = () => {
