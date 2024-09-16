40babba5465b17bc2dfb1df13429fd870fdd71bc
David  Anusontarangkul <davidanusontarangkul@Davids-MacBook-Pro.local>
2024-09-15 18:17:04 -0700

    add promo codes

---
+++ convex/promoCode.ts
@@ -0,0 +1,18 @@
+import { internalMutation } from "./_generated/server";
+import { v } from "convex/values";
+
+export const findPromoIdByCode = internalMutation({
+  args: { promoCode: v.string() },
+  handler: async ({ db }, { promoCode }) => {
+    const promo = await db
+      .query("promoCodes")
+      .filter((q) => q.eq(q.field("promoCode"), promoCode))
+      .first();
+
+    if (!promo) {
+      throw new Error("Promo code not found");
+    }
+
+    return promo;
+  },
+});
