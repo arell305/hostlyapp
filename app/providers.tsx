637eeeefddede8c0dc9822871de6cc6596727cb7
David  Anusontarangkul <anusontarangkul.d@gmail.com>
2024-09-17 17:06:48 -0700

    update providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,15 +6,25 @@ import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { PropsWithChildren } from "react";
 import { ThemeProvider } from "./theme-provider";
 
-const convex = new ConvexReactClient(
-  process.env.NEXT_PUBLIC_CONVEX_URL as string
-);
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL as string;
+const clerkPublishableKey = process.env
+  .NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
+
+if (!convexUrl) {
+  throw new Error("NEXT_PUBLIC_CONVEX_URL environment variable is not set.");
+}
+
+if (!clerkPublishableKey) {
+  throw new Error(
+    "NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY environment variable is not set."
+  );
+}
+
+const convex = new ConvexReactClient(convexUrl);
 
 export function Providers({ children }: PropsWithChildren) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider publishableKey={clerkPublishableKey}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
       </ConvexProviderWithClerk>
