539cc948de0fab8715df2cfd8bd47923273fcf76
David  Anusontarangkul <anusontarangkul.d@gmail.com>
2025-03-18 18:31:47 -0700

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,9 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_CONVEX_DOMAIN,
+        hostname:
+          process.env.NEXT_PUBLIC_CONVEX_DOMAIN ||
+          "oceanic-tapir-499.convex.cloud",
         port: "",
         pathname: "/**",
       },
