f2eb91ba3006681b7ee9d188dafb64d5058097c3
David  Anusontarangkul <anusontarangkul.d@gmail.com>
2025-11-12 18:49:53 +0700

    removed comments

--- app/components/shared/containers/EventGrid.tsx
+++ app/components/shared/containers/EventGrid.tsx
@@ -1,23 +1,3 @@
-// "use client";
-
-// import React from "react";
-
-// interface EventGridProps {
-//   children: React.ReactNode;
-//   className?: string;
-// }
-
-// const EventGrid: React.FC<EventGridProps> = ({ children, className = "" }) => {
-//   return (
-//     <div
-//       className={`w-full grid grid-cols-1 md:grid-cols-3  gap-y-8 gap-x-4 justify-items-center ${className}`}
-//     >
-//       {children}
-//     </div>
-//   );
-// };
-
-// export default EventGrid;
 "use client";
 
 import React from "react";
