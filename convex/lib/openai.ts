10b602dc878e382461f311e34e923f1420d18d98
David  Anusontarangkul <anusontarangkul.d@gmail.com>
2025-11-28 19:40:05 +0700

    comment out

--- convex/lib/openai.ts
+++ convex/lib/openai.ts
@@ -1,9 +1,9 @@
 import OpenAI from "openai";
 
 const openai = (() => {
-  if (!process.env.OPENAI_API_KEYa) {
-    throw new Error("OPENAI_API_KEY is missing in .env.local");
-  }
+  //   if (!process.env.OPENAI_API_KEYa) {
+  //     throw new Error("OPENAI_API_KEY is missing ");
+  //   }
   return new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 })();
 
